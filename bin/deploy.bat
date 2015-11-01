887f9ffc855f7cc69abc7d243b78c5f92ea3a76e
Birkir Freyr <birkirfb@i-7f3b5dd9.csd07cloud.internal>
2015-11-01 23:41:00 +0000

    now the deploy script is deploying multiple files to target outside working dir.

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,18 +1,30 @@
 set JAR="TicTacToe-SoftwareBoys.jar"
+set TAR="TicTacToe-SoftwareBoys.tar"
+set ZIP="TicTacToe-SoftwareBoys.zip"
+
 set LOCATION="C:\temp"
+
+
 
 REM Always deploy new version, clean before
 call bin\clean.bat
 call bin\build.bat
 call bin\package.bat
 
-REM If jar file exists under C:\temp, remove it
-if exist %LOCATION%%JAR% del /F %LOCATION%%JAR%
+REM If directory exists, remove it
+if exist %LOCATION% del /F %LOCATION%
 
 REM If destionation folder doesn't exist, create it
 if not exist "%LOCATION%" mkdir %LOCATION%
 
+
+
 copy build\libs\%JAR% %LOCATION%
+copy build\distributions\%TAR% %LOCATION%
+copy build\distributions\%ZIP% %LOCATION%
+xcopy build\reports %LOCATION% /s /e /y
+
 
 REM Run application once
 java -jar %LOCATION%\\%JAR%
+
